1386345f1472ad56ddec74c005d0e386c2cc66de
Андрей Осьмаков <38988068+osmaav@users.noreply.github.com>
2024-03-15 19:43:20 +0700

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
 const OATUH_APP_ID = '4d583690e1214c4581e492a653213c59';
 
-const TOKEN_KEY = 'yandex-music-token';
+const TOKEN_KEY = 'yandex-token';
 
 const OUATH_URL = `https://oauth.yandex.ru/authorize?response_type=token&client_id=${OATUH_APP_ID}`;
-const URL_WITH_ACCESS_TOKEN_REGEX = 'https:\\/\\/music\\.yandex\\.(?:ru|com|by|kz|ua)\\/#access_token=([^&]*)';
+const URL_WITH_ACCESS_TOKEN_REGEX = 'https:\\/\\/music\\.yandex\\.(?:ru|com)\\/#access_token=([^&]*)';
 
 let _oauthTabId;
 
@@ -22,10 +22,9 @@ chrome.tabs.onUpdated.addListener((tabId, _, tab) => {
   const match = tab.url.match(URL_WITH_ACCESS_TOKEN_REGEX);
 
   if (match) {
-    chrome.storage.local.set({[TOKEN_KEY]: match[1]});
+    localStorage.setItem(TOKEN_KEY,match[1]);
     // close tab
     chrome.tabs.remove(_oauthTabId);
-
     _oauthTabId = null;
   }
 });
