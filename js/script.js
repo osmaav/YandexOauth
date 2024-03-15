9fc7c6e0e7516d4d2a09cd18106d715fe89561dc
Андрей Осьмаков <38988068+osmaav@users.noreply.github.com>
2024-03-15 22:02:28 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,15 +6,7 @@ const OATUH_APP_ID = '4d583690e1214c4581e492a653213c59';
          response_type: 'token',
          redirect_uri: 'https://osmaav.github.io/YandexOauth/'
       },
-      'https://osmaav.github.io/YandexOauth/', 
-        {
-            view: 'button',
-            parentId: 'container',
-            buttonView: 'main',
-            buttonTheme: 'light',
-            buttonSize: 'm',
-            buttonBorderRadius: 0
-         }
+      'https://osmaav.github.io/YandexOauth/'
        ).then(function(result) {
                return result.handler()
             })
